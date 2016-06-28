a21f3a82a959c8e8883bda46cd5bf9620ad6b6e2
UmutOrman <umutormans@gmail.com>
2016-06-28 18:03:38 +0300

    Example pip-installable project

---
+++ pip_project/fun.py
@@ -0,0 +1,9 @@
+from random import randint
+
+def fun():
+    result = randint(0,100)
+    return result
+
+def more_fun():
+    x = fun() - 100
+    return x
---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+ 
+setup(
+    name = 'pipfun',
+    packages = ['pip_project'],
+    version = '0.0.1',
+    description = 'Does nothing much',
+    author='Umut Orman',
+    author_email='umutormans@gmail.com',
+    url='https://github.com/youraccount/foolib',
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: OS Independent',
+        'Development Status :: 1 - Planning',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'Topic :: ComputerScience/Engineering :: CS'
+    ]
+)
